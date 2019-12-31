d6406147d4f8ec4877e79429d59f7a7856a83bc3
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-31 13:02:38 +0300

    Added project

---
+++ DesignPatterns_FactoryMethod/DesignPatterns_FactoryMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Factory Method!");
+        }
+    }
+}
